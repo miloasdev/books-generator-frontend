7d462a2a0e50eeacf94b2648bf2b68e958098cd3
MiloAsDev <miloasdev@gmail.com>
2025-08-21 16:26:33 +0500

    App rendering bug fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../index.css'
-import { AppRouter } from './app/router'
+import { router } from './app/router'
+import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <AppRouter />
+        <RouterProvider router={router} />
     </StrictMode>,
 )
